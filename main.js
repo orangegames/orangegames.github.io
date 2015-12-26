e7efc0b0bd28ee89cb1e02290e1d11ff23eac1ce
Hao Lin <linhao@chukong-inc.com>
2015-12-26 09:31:48 +0800

    aaa

--- main.js
+++ main.js
@@ -23,27 +23,25 @@ cc.game.onStart = function(){
         var returnUrl = window.location.href;
 
         returnUrl = encodeURIComponent(returnUrl);
-        window.location.href = "http://www.yinshuiyu.com/api/wx_login?fid=" + fid + "&return_url=" + returnUrl;
-        return;
+        window.location.href = "http://www.yinshuiyu.com/api/wx_login?fuid=" + fid + "&return_url=" + returnUrl;
+    }else{
+        cc.sys.localStorage.setItem("login_tag", 0);
+        if(!cc.sys.isNative && document.getElementById("cocosLoading")) //If referenced loading.js, please remove it
+            document.body.removeChild(document.getElementById("cocosLoading"));
+
+        // Pass true to enable retina display, disabled by default to improve performance
+        cc.view.enableRetina(false);
+        // Adjust viewport meta
+        cc.view.adjustViewPort(true);
+        // Setup the resolution policy and design resolution size
+        cc.view.setDesignResolutionSize(ScreenSize.width, ScreenSize.height, cc.ResolutionPolicy.SHOW_ALL);
+        // The game will be resized when browser size change
+        cc.view.resizeWithBrowserSize(true);
+        //load resources
+        cc.LoadingScene.preload(g_resources, function () {
+            cc.director.runScene(new StartScene());
+        }, this);
     }
-
-
-    cc.sys.localStorage.setItem("login_tag", 0);
-    if(!cc.sys.isNative && document.getElementById("cocosLoading")) //If referenced loading.js, please remove it
-        document.body.removeChild(document.getElementById("cocosLoading"));
-
-    // Pass true to enable retina display, disabled by default to improve performance
-    cc.view.enableRetina(false);
-    // Adjust viewport meta
-    cc.view.adjustViewPort(true);
-    // Setup the resolution policy and design resolution size
-    cc.view.setDesignResolutionSize(ScreenSize.width, ScreenSize.height, cc.ResolutionPolicy.SHOW_ALL);
-    // The game will be resized when browser size change
-    cc.view.resizeWithBrowserSize(true);
-    //load resources
-    cc.LoadingScene.preload(g_resources, function () {
-        cc.director.runScene(new StartScene());
-    }, this);
 };
 cc.game.run();
 
